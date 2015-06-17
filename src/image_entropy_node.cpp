fed23771461be7311d289ff76cdaf11ce66a7543
Alex Wallar <aw204@st-andrews.ac.uk>
2015-06-17 09:15:47 -0400

    test

--- src/image_entropy_node.cpp
+++ src/image_entropy_node.cpp
@@ -11,13 +11,24 @@ const int height = 480;
 const int num_events = 255 / 10 + 1;
 int occs [width][height][num_events];
 
+void initialize_occs(int occs[width][height][num_events]) {
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            for (int k = 0; k < num_events; k++) {
+                occs[i][j][k] = 0;
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
 
     ros::init(argc, argv, "image_entropy_node");
     ros::NodeHandle n;
-    ros::Time::init();
 
+    initialize_occs(occs);
     VideoCapture cap(0);
+
     if(!cap.isOpened()) {
         return -1;
     }
@@ -31,7 +42,7 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 int event = frame.at<int>(j, i) / 10;
-                ROS_DEBUG_THROTTLE(1, "%d", event);
+                ROS_DEBUG_THROTTLE(10, "%d", event);
                 // occs[i][j][event]++;
             }
         }
