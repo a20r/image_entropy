59f9370bc29a3501831f636bee2c08de60581c25
Alex Wallar <aw204@st-andrews.ac.uk>
2015-06-18 13:15:13 -0400

    Rossify

--- include/image_entropy_node.hpp
+++ include/image_entropy_node.hpp
@@ -5,8 +5,10 @@ const int interval = 10;
 const int num_events = 255 / interval + 1;
 const int update_threshold = num_events;
 const double learning_rate = 0.4;
+const int queue_size = 1;
 
 void initialize_occs(int occs[width][height][num_events]);
 double calculate_entropy(int occs[width][height][num_events], int x, int y);
 int sum_row(int occs[width][height][num_events], int x, int y);
 void reset_occs(int occs[width][height][num_events], int x, int y);
+void pose_callback(geometry_msgs::Pose);
--- src/image_entropy_node.cpp
+++ src/image_entropy_node.cpp
@@ -1,6 +1,7 @@
 
 #include "ros/ros.h"
 #include "ros/console.h"
+#include "geometry_msgs/Pose.h"
 #include "opencv2/opencv.hpp"
 #include "image_entropy_node.hpp"
 #include "tf/tf.h"
@@ -11,13 +12,17 @@ using namespace std;
 
 int occs [width][height][num_events];
 double entropy_grid[width][height] = {1};
+string pose_topic;
+string image_entropy_topic = "/image_entropy/entropy";
+VideoCapture cap;
+ros::Subscriber pose_sub;
+Mat frame, e_surf;
+
 
 void initialize_occs(int occs[width][height][num_events]) {
     for (int i = 0; i < width; i++) {
         for (int j = 0; j < height; j++) {
-            for (int k = 0; k < num_events; k++) {
-                occs[i][j][k] = 0;
-            }
+            reset_occs(occs, i, j);
         }
     }
 }
@@ -64,51 +69,52 @@ void develop_entropy_image(double eg[width][height], Mat *dst) {
     }
 }
 
+void pose_callback(geometry_msgs::Pose) {
+    cap >> frame;
+    cvtColor(frame, frame, CV_BGR2GRAY);
+    resize(frame, frame, Size(width, height));
+    resize(e_surf, e_surf, Size(width, height));
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            Scalar intensity = frame.at<uchar>(j, i);
+            int event = intensity.val[0] / interval;
+            occs[i][j][event]++;
+
+            if (sum_row(occs, i, j) >= update_threshold) {
+                double oe = (1 - learning_rate) * entropy_grid[i][j];
+                double ne = learning_rate * calculate_entropy(occs, i, j);
+                entropy_grid[i][j] = oe + ne;
+                reset_occs(occs, i, j);
+            }
+        }
+    }
+
+    cvtColor(e_surf, e_surf, CV_BGR2HSV);
+    develop_entropy_image(entropy_grid, &e_surf);
+    cvtColor(e_surf, e_surf, CV_HSV2BGR);
+    GaussianBlur(e_surf, e_surf, Size(5, 5), 0, 0);
+    resize(e_surf, e_surf, Size(640, 480));
+    imshow("Entropy", e_surf);
+    waitKey(1);
+    ROS_DEBUG_THROTTLE(1, "Test");
+}
 
 int main(int argc, char *argv[]) {
 
     ros::init(argc, argv, "image_entropy_node");
     ros::NodeHandle n;
 
+    namedWindow("Entropy", 1);
     initialize_occs(occs);
-    VideoCapture cap(0);
+    cap = VideoCapture(0);
+    cap >> e_surf;
 
     if(!cap.isOpened()) {
         return -1;
     }
 
-    Mat frame;
-    Mat e_surf;
-    cap >> e_surf;
-    namedWindow("Entropy", 1);
-
-    while (waitKey(1) < 0) {
-        cap >> frame;
-        cvtColor(frame, frame, CV_BGR2GRAY);
-        resize(frame, frame, Size(width, height));
-        resize(e_surf, e_surf, Size(width, height));
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                Scalar intensity = frame.at<uchar>(j, i);
-                int event = intensity.val[0] / interval;
-                occs[i][j][event]++;
-
-                if (sum_row(occs, i, j) >= update_threshold) {
-                    double oe = (1 - learning_rate) * entropy_grid[i][j];
-                    double ne = learning_rate * calculate_entropy(occs, i, j);
-                    entropy_grid[i][j] = oe + ne;
-                    reset_occs(occs, i, j);
-                }
-            }
-        }
-
-        cvtColor(e_surf, e_surf, CV_BGR2HSV);
-        develop_entropy_image(entropy_grid, &e_surf);
-        cvtColor(e_surf, e_surf, CV_HSV2BGR);
-        GaussianBlur(e_surf, e_surf, Size(5, 5), 0, 0);
-        resize(e_surf, e_surf, Size(640, 480));
-        imshow("Entropy", e_surf);
-    }
+    ros::param::get("~pose_topic", pose_topic);
+    pose_sub = n.subscribe(pose_topic, queue_size, pose_callback);
+    ros::spin();
     return 0;
-
 }
