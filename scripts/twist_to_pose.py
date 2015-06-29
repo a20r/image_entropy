fd14b9def99ef1bd45608609ccc72c89849b6da7
Alex Wallar <aw204@st-andrews.ac.uk>
2015-06-29 04:49:45 -0400

    Working better but still not done

--- scripts/twist_to_pose.py
+++ scripts/twist_to_pose.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 
 import rospy
+import math
+import tf
 from geometry_msgs.msg import PoseStamped, Twist
 
 ps = PoseStamped()
 counter = 0
+beta = 0
 pose_pub = None
 frame_id = None
 altitude = None
 
 
 def twist_callback(twist):
-    global ps, pose_pub, counter, frame_id
+    global ps, pose_pub, counter, frame_id, beta
     ps.header.seq = counter
     ps.header.stamp = rospy.Time.now()
     ps.header.frame_id = frame_id
     ps.pose.position.x += twist.linear.x
     ps.pose.position.y += twist.linear.y
     ps.pose.position.z = altitude
-    ps.pose.orientation.x += twist.angular.x
-    ps.pose.orientation.y += twist.angular.y
-    ps.pose.orientation.z += twist.angular.z
+    beta += math.radians(4 * twist.angular.z)
+    q = tf.transformations.quaternion_from_euler(0, 0, beta)
+    ps.pose.orientation.x = q[0]
+    ps.pose.orientation.y = q[1]
+    ps.pose.orientation.z = q[2]
+    ps.pose.orientation.w = q[3]
     pose_pub.publish(ps)
     counter += 1
 
